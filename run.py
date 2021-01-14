30d0f6ea2cc8c700878539aa5c8de3c7856c08f9
Ana Tulea <anatulea14@gmail.com>
2021-01-13 17:53:49 -0800

    test

--- run.py
+++ run.py
@@ -6,11 +6,11 @@ if __name__ == "__main__":
     flask_app = create_app('prod')
     with flask_app.app_context(): # to ensure that the SQLAlchemy binds with my application(there may be multiple apps)
         db.create_all()
-        try:
+        
         # create a default user if there is no user(create_user() from app/auth/models.py)
-            if not User.query.filter_by(user_name='harry').first():
-                User.create_user(user='harry',
+        if not User.query.filter_by(user_name='harry').first():
+            User.create_user(user='harry',
                                 email='harry@potters.com',
                                 password='secret')
-        except exc.IntegrityError:
-            flask_app.run()	  
+    
+        flask_app.run()  
